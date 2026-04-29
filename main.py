cf544ace4d1986a91cf3c1bcc5406aabe067457a
danilofarias8722-netizen <danilofarias8722@gmail.com>
2026-04-29 13:14:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import discord
+from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Bot online: {client.user}')
+    print(f'Bot online: {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('!ping'):
-        await message.channel.send('Pong!')
+@bot.command()
+async def loja(ctx):
+    embed = discord.Embed(
+        title="🛍️ LOJA DRIP",
+        description="Clique no botão abaixo para acessar a loja:",
+        color=0x00ff00
+    )
+    await ctx.send(embed=embed)
 
 TOKEN = os.getenv('TOKEN')
-client.run(TOKEN)
+bot.run(TOKEN)
