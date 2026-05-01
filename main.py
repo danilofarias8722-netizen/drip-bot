31110204d08eea9fedbaf317b2e5e93aa0ea94ed
danilofarias8722-netizen <danilofarias8722@gmail.com>
2026-05-01 11:22:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ tree = app_commands.CommandTree(bot)
 
 @tree.command(name="loja", description="Abre a loja do servidor")
 async def loja(interaction: discord.Interaction):
-    await interaction.response.send_message("Loja em construção...",ephemeral=True )
+    await interaction.response.send_message("Loja em construção...", ephemeral=True)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     
-    GUILD_ID = 1498859204696346755  # ID do teu servidor
+    GUILD_ID = 1498859204696346755
     guild = discord.Object(id=GUILD_ID)
     
     tree.clear_commands(guild=guild)
     await tree.sync(guild=guild)
     
     print(f'Synced commands to guild {GUILD_ID}')
+
 bot.run(os.getenv("DISCORD_TOKEN"))
