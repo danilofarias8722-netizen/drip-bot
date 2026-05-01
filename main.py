2a2a8528f998649de8e62e83361f4e1a5f80c01d
danilofarias8722-netizen <danilofarias8722@gmail.com>
2026-05-01 17:06:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ tree = app_commands.CommandTree(bot)
 
 @bot.event
 async def on_ready():
-    # Registra o comando só no teu servidor - instantâneo
-    await tree.sync(guild=discord.Object(id=1498859204696346755))
-    print(f'Bot {bot.user} online!')
-    print('/loja registrado com sucesso!')
+    # Pega o ID da Variable GUILD_ID do Railway
+    guild_id = int(os.getenv("GUILD_ID"))
+    await tree.sync(guild=discord.Object(id=guild_id))
+    print(f'Bot {bot.user} tá online!')
+    print('Comando /loja registrado!')
 
 @tree.command(name="loja", description="Abre a loja do servidor")
 async def loja(interaction: discord.Interaction):
-    await interaction.response.send_message("Loja funcionando! 🛒")
+    await interaction.response.send_message("A loja tá aberta! 🛒")
 
-# Pega o token escondido do Railway
+# Pega o TOKEN da Variable do Railway
 bot.run(os.getenv("TOKEN"))
