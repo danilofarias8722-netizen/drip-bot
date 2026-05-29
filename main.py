9ddd84e9d77020d7a9982b8462cdaa55f813eff7
danilofarias8722-netizen <danilofarias8722@gmail.com>
2026-05-29 08:59:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import datetime
+import os
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
